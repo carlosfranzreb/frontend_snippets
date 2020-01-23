24069fd3620b181f726ffd32a208a974503748f4
charlo <30505353+carlosfranzreb@users.noreply.github.com>
2020-01-23 14:47:07 +0100

    Bugfix: some angles were wrong.

--- arrows.js
+++ arrows.js
@@ -7,34 +7,54 @@
 
 
 function arrow(canvas, from_x, from_y, to_x, to_y, width) {
-    console.log(width);
-    console.log(get_offset(width));
     let angle = Math.atan2(to_y-from_y, to_x-from_x);  // Angle of the arrow.
-    let headlen = width + (33 - width / 3); // Length of head in pixels
+    let head_length = width + (33 - width / 3); // Length of head in pixels
     let head_offset = Math.PI / get_offset(width);  // angle offset between line and head.
-    let head_base = to_y - headlen * Math.sin(angle - head_offset);  // Where the head starts (y-coordinate).
-    let left_corner = to_x - headlen * Math.cos(angle - head_offset);  // Left corner of the head.
-    let right_corner = to_x - headlen * Math.cos(angle + head_offset);  // Right corner of the head.
-    // Draw the line of the arrow:
+    let left_corner = [
+        to_x - head_length * Math.cos(angle - head_offset),
+        to_y - head_length * Math.sin(angle - head_offset)
+    ];  // Left corner of the head.
+    let right_corner = [
+        to_x - head_length * Math.cos(angle + head_offset),
+        to_y - head_length * Math.sin(angle + head_offset)
+    ];  // Right corner of the head.
+    let head_base = [
+        (right_corner[0] + left_corner[0]) / 2,
+        (right_corner[1] + left_corner[1]) / 2
+    ];  // Where the head starts.
+    
     let ctx = canvas.getContext('2d');
     ctx.beginPath();
-    ctx.strokeStyle = "#e8ad00";
+    // Draw the line of the arrow:
     ctx.lineWidth = width;
+    ctx.strokeStyle = "#e8ad00";
     ctx.moveTo(from_x, from_y);
-    ctx.lineTo(to_x, head_base);
+    ctx.lineTo(head_base[0], head_base[1]);
     ctx.stroke();
     // Draw the head of the arrow:
     ctx.lineWidth = 1;
     ctx.fillStyle = "#e8ad00";
     ctx.moveTo(to_x, to_y);
-    ctx.lineTo(left_corner, head_base);
-    ctx.moveTo(to_x, to_y);
-    ctx.lineTo(right_corner, head_base);
-    ctx.lineTo(right_corner, head_base);
-    ctx.lineTo(left_corner, head_base);
+    ctx.lineTo(left_corner[0], left_corner[1]);
+    ctx.lineTo(right_corner[0], right_corner[1]);
+    ctx.lineTo(to_x, to_y);
     ctx.fill();
 }
 
+function get_offset(width) {
+    switch (true) {
+        case (width < 3):
+            return 10;
+        case (width < 15):
+            return 8;
+        case (width < 40):
+            return 5;
+        case (width < 70):
+            return 4;
+        default:
+            return 3;
+    }
+}
 
 function down_arrow(canvas, width) {
     let x = canvas.width / 2;
@@ -55,20 +75,4 @@ function right_arrow(canvas, width) {
     let from_x = 0;
     let to_x = canvas.width;
     arrow(canvas, from_x, y, to_x, y, width);
-}
-
-
-function get_offset(width) {
-    switch (true) {
-        case (width < 3):
-            return 10;
-        case (width < 15):
-            return 8;
-        case (width < 40):
-            return 5;
-        case (width < 70):
-            return 4;
-        default:
-            return 3;
-    }
 }
